2ae888683d37a4b6df6228844ae39acd0edd89a0
Sediel Ruiz <Sedielruiz21@gmail.com>
2024-11-28 22:56:46 -0500

    Fix: config migrations

--- src/config/typeorm-migrations.config.ts
+++ src/config/typeorm-migrations.config.ts
@@ -1,11 +1,14 @@
 import { DataSource } from "typeorm"
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 export default new DataSource({
     type: 'postgres',
-    host: 'to-do-list-postgres',
-    port: 5432,
-    username: 'postgres',
-    password: '123456789',
-    database: 'to_do_list',
+    host: process.env.db_host,
+    port: parseInt(process.env.db_port),
+    username: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_name,
     entities: [__dirname + "/../**/*.entity{.ts,.js}"],
     migrations: ['src/domain/migrations/*{.ts,.js}']
 })
